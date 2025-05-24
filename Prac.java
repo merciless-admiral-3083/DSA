ca71f17bdaadb67c497d27a1359b517fc4127315
Jaspreet Singh Nahal <jaspreetnahal100@gmail.com>
2025-05-24 16:23:58 +0530

    p

--- Prac.java
+++ Prac.java
@@ -2,25 +2,6 @@ import java.util.Scanner;
 
 public class Prac {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter number of elements: ");
-        int nope = input.nextInt();
-        int[] arroid = new int[nope];
-
-        System.out.println("Enter " + nope + " elements:");
-        for (int io = 0; io < nope; io++) {
-            arroid[io] = input.nextInt();
-        }
-
-        // Initialize max with first element
-        int maximum = arroid[0];
-        for (int iop = 1; iop < arroid.length; iop++) {
-            if (arroid[iop] > maximum) {
-                maximum = arroid[iop];
-            }
-        }
-
-        System.out.println("Maximum value: " + maximum);
-        input.close();
+        
     }
 }
